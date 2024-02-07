b364a01b7a98adb4303b3e503148f97b58a4c493
jhon centeno <jcenteno.ing@gmail.com>
2024-02-07 08:40:39 -0400

    Acceso a seccion galeria mediante ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main-components/main.component';
+import { GalleryComponent } from './main-components/gallery/gallery.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: MainComponent
+    },
+    {
+        path: 'gallery',
+        component: GalleryComponent
     },
 ];
