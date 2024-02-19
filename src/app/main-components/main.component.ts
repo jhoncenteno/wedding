15353c3c44472335a03dc8d5863481041538f1df
jhon centeno <jcenteno.ing@gmail.com>
2024-02-19 09:39:23 -0400

    Contador regresivo sigue con problemas

--- src/app/main-components/main.component.ts
+++ src/app/main-components/main.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 // import { Loader } from '@googlemaps/js-api-loader';
 
+import { Observable, Subject, interval } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
+
 import moment, { Duration, Moment } from 'moment';
 
 @Component({
@@ -9,65 +12,75 @@ import moment, { Duration, Moment } from 'moment';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-  fechaEspecifica: string = '2024-12-31'; // Fecha específica para la que deseas calcular el contador
-  fechaEspecificaMoment: Moment = moment(this.fechaEspecifica)
-  fechaActual: any
-  diasRestantes: number = 0;
-  horasRestantes: number = 0;
-  minutosRestantes: number = 0;
-  segundosRestantes: number = 0;
 
+  // ngOnInit(): void {
+  //   // ------------------
+  //   // let map: google.maps.Map;
 
+  //   // const center = { lat: 41.90476224706472, lng: 12.49822074385094 };
+  //   // const zoom = 14;
+  //   // const url = "https://maps.googleapis.com/maps/api/staticmap";
+    
+  //   // // @ts-ignore google.maps.plugins
+  //   // const loader = new Loader({
+  //   //   apiKey: "AIzaSyDUbmmjq-GQq7GwEMW609SUV2XnNBGIK28",
+  //   //   version: "weekly",
+  //   // });
+    
+  //   // document.addEventListener("DOMContentLoaded", () => {
+  //   //   const wrapper = document.getElementById("wrapper") as HTMLButtonElement;
+    
+  //   //   wrapper.style.backgroundImage = `url(${url}?center=${center.lat},${center.lng}&zoom=${zoom}&scale=2&size=${wrapper.clientWidth}x${wrapper.clientHeight}&key=YOUR_API_KEY)`;
+    
+  //   //   wrapper.addEventListener("click", () => {
+  //   //     wrapper.remove();
+    
+  //   //     loader.load().then(() => {
+  //   //       map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+  //   //         center,
+  //   //         zoom,
+  //   //       });
+  //   //     });
+  //   //   });
+  //   // });
+  //   // ------------------
+
+  // }
+
+  // -----------------------------
+  targetDate = new Date('2024-12-31T23:59:59'); // Reemplaza con tu fecha objetivo
+  public days: number = 0;
+  public hours: number = 0;
+  public minutes: number = 0;
+  public seconds: number = 0;
 
   constructor() { }
 
-  ngOnInit(): void {
-    // ------------------
-    // let map: google.maps.Map;
-
-    // const center = { lat: 41.90476224706472, lng: 12.49822074385094 };
-    // const zoom = 14;
-    // const url = "https://maps.googleapis.com/maps/api/staticmap";
-    
-    // // @ts-ignore google.maps.plugins
-    // const loader = new Loader({
-    //   apiKey: "AIzaSyDUbmmjq-GQq7GwEMW609SUV2XnNBGIK28",
-    //   version: "weekly",
-    // });
-    
-    // document.addEventListener("DOMContentLoaded", () => {
-    //   const wrapper = document.getElementById("wrapper") as HTMLButtonElement;
-    
-    //   wrapper.style.backgroundImage = `url(${url}?center=${center.lat},${center.lng}&zoom=${zoom}&scale=2&size=${wrapper.clientWidth}x${wrapper.clientHeight}&key=YOUR_API_KEY)`;
-    
-    //   wrapper.addEventListener("click", () => {
-    //     wrapper.remove();
-    
-    //     loader.load().then(() => {
-    //       map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    //         center,
-    //         zoom,
-    //       });
-    //     });
-    //   });
-    // });
-    // ------------------
-
-    // console.log("Aquiiiiiii")
-    this.actualizarContador(); // Llama a la función para que el contador se actualice inmediatamente
+  ngOnInit() {
+    console.log(" AQUIIIIII")
+    this.initCountdown();
     // setInterval(() => {
-    //   console.log("Acaaa")
-    //   this.actualizarContador(); // Llama a la función cada segundo para mantener actualizado el contador
+    //   this.initCountdown();
     // }, 1000);
   }
 
-  actualizarContador(): void {    
-    this.fechaActual = moment();
-    const diferencia = moment.duration(this.fechaEspecificaMoment.diff(this.fechaActual));
-
-    this.diasRestantes = diferencia.days();
-    this.horasRestantes = diferencia.hours();
-    this.minutosRestantes = diferencia.minutes();
-    this.segundosRestantes = diferencia.seconds();
+  initCountdown() {
+    this.days = this.calculateRemainingTime().days;
+    this.hours = this.calculateRemainingTime().hours;
+    this.minutes = this.calculateRemainingTime().minutes;
+    this.seconds = this.calculateRemainingTime().seconds;
   }
+
+  calculateRemainingTime(): { days: number, hours: number, minutes: number, seconds: number } {
+    const now = new Date();
+    const diff = this.targetDate.getTime() - now.getTime();
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    return { days, hours, minutes, seconds };
+  }
+
 }
