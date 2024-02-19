8dffca8f8f95f74d33968e7becc4d2798777829d
jhon centeno <jcenteno.ing@gmail.com>
2024-02-19 11:01:30 -0400

    configurando .env

--- src/app/main-components/main.component.ts
+++ src/app/main-components/main.component.ts
@@ -5,6 +5,8 @@ import { Observable, Subject, interval } from 'rxjs';
 import { map, takeUntil } from 'rxjs/operators';
 
 import moment, { Duration, Moment } from 'moment';
+import * as dotenv from 'dotenv';
+
 
 @Component({
   selector: 'app-main',
@@ -13,36 +15,45 @@ import moment, { Duration, Moment } from 'moment';
 })
 export class MainComponent implements OnInit {
 
-  // ngOnInit(): void {
-  //   // ------------------
-  //   // let map: google.maps.Map;
 
-  //   // const center = { lat: 41.90476224706472, lng: 12.49822074385094 };
-  //   // const zoom = 14;
-  //   // const url = "https://maps.googleapis.com/maps/api/staticmap";
+  // ngOnInit(): void {
+  //   dotenv.config(); // Lee el archivo .env
+  //   let apiKey = ""
+  //   if (process.env["GOOGLE_MAPS_API_KEY"]) {
+  //     apiKey = process.env["GOOGLE_MAPS_API_KEY"];
+  //   } else {
+  //     console.error("API key not found in .env");
+  //   }
+
+  //   // ------------------
+  //   let map: google.maps.Map;
+
+  //   const center = { lat: 41.90476224706472, lng: 12.49822074385094 };
+  //   const zoom = 14;
+  //   const url = "https://maps.googleapis.com/maps/api/staticmap";
     
-  //   // // @ts-ignore google.maps.plugins
-  //   // const loader = new Loader({
-  //   //   apiKey: "AIzaSyDUbmmjq-GQq7GwEMW609SUV2XnNBGIK28",
-  //   //   version: "weekly",
-  //   // });
+  //   // @ts-ignore google.maps.plugins
+  //   const loader = new Loader({
+  //     apiKey,
+  //     version: "weekly",
+  //   });
     
-  //   // document.addEventListener("DOMContentLoaded", () => {
-  //   //   const wrapper = document.getElementById("wrapper") as HTMLButtonElement;
+  //   document.addEventListener("DOMContentLoaded", () => {
+  //     const wrapper = document.getElementById("wrapper") as HTMLButtonElement;
     
-  //   //   wrapper.style.backgroundImage = `url(${url}?center=${center.lat},${center.lng}&zoom=${zoom}&scale=2&size=${wrapper.clientWidth}x${wrapper.clientHeight}&key=YOUR_API_KEY)`;
+  //     wrapper.style.backgroundImage = `url(${url}?center=${center.lat},${center.lng}&zoom=${zoom}&scale=2&size=${wrapper.clientWidth}x${wrapper.clientHeight}&key=YOUR_API_KEY)`;
     
-  //   //   wrapper.addEventListener("click", () => {
-  //   //     wrapper.remove();
+  //     wrapper.addEventListener("click", () => {
+  //       wrapper.remove();
     
-  //   //     loader.load().then(() => {
-  //   //       map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-  //   //         center,
-  //   //         zoom,
-  //   //       });
-  //   //     });
-  //   //   });
-  //   // });
+  //       loader.load().then(() => {
+  //         map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+  //           center,
+  //           zoom,
+  //         });
+  //       });
+  //     });
+  //   });
   //   // ------------------
 
   // }
