2bacb55d354060df503317e9fe92538729126484
jhon centeno <jcenteno.ing@gmail.com>
2024-02-21 11:16:40 -0400

    configuracion de firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration(), 
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebase),),
+      provideFirestore(() => getFirestore()),
+      provideStorage(() => getStorage())
+    ), 
+
+  ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        projectId:"weddingpl-3552d",
+        appId:"1:972383149854:web:a3d45e71853ffb46e0f30f",
+        storageBucket:"weddingpl-3552d.appspot.com",
+        // locationId:"us-central",
+        apiKey:"AIzaSyDUbmmjq-GQq7GwEMW609SUV2XnNBGIK28",
+        authDomain:"weddingpl-3552d.firebaseapp.com",
+        messagingSenderId:"972383149854"
+    }
+};
